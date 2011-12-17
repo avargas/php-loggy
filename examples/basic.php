04754b68b604148ced500563164ae2eb9025a3f3
Angelo Vargas <angelo@nivler.com>
2011-12-17 13:25:16 -0600

    fixed example

--- examples/basic.php
+++ examples/basic.php
@@ -7,19 +7,17 @@ use loggy;
 require_once __DIR__ . '/../src/loggy/Autoloader.php';
 
 
-loggy\Logger::factory();
+$writer = new loggy\writer\CommandLineWriter;
+$writer->setFormatter(new loggy\formatter\CommandLineFormatter);
 
+loggy\Logger::setWriter($writer);
+
+
+# get facility logger space
 $logger = loggy\Logger::get();
-
 $logger->debug('debug');
 $logger->error('an error');
 $logger->warn('warning!');
 $logger->info('info');
 
 $logger->fatal('fatal message on %s', basename(__FILE__));
-
-$messages = loggy\formatter\SimpleFormatter::formatLoggerMessages(loggy\Logger::getInstance());
-
-foreach ($messages as $msg) {
-	echo $msg . "\n";
-}
