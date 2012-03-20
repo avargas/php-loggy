3928a4a2c11f9dd1f2c54735faf055b10f6cf5be
Angelo Vargas <angelo@at.com>
2012-03-20 18:35:34 -0500

    fixed composer psr-0 and autoloading issue

--- src/loggy/Autoloader.php
+++ src/loggy/Autoloader.php
@@ -6,24 +6,6 @@ define('LOGGY_START_MS', microtime(true));
 
 $vendor = __DIR__ . '/../../vendor/.composer/autoload.php';
 
-if (is_dir($vendor)) {
-	require_once $vendor . '/.composer/autoload.php';
+if (file_exists($vendor)) {
+	require_once $vendor;
 }
-
-spl_autoload_register(function ($class) {
-	if ($class == 'Colors') {
-		$file = realpath(__DIR__ . '/../../vendor/') . '/Bash-Color/colors.class.php';
-		goto test;
-	}
-
-	if (strpos($class, 'loggy') !== 0) {
-		return;
-	}
-
-	$file = realpath(__DIR__ . '/../') . '/' . str_replace('\\', '/', $class) . '.php';
-
-	test:
-	if (file_exists($file)) {
-		require_once $file;
-	}
-});
